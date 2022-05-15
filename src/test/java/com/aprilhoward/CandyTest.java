d9ec58bf16ca80c0c49f5c52c43df67446014cad
April Howard <apehow111@gmail.com>
2022-05-15 12:51:32 -0400

    Test CSVLoader

--- src/test/java/com/aprilhoward/CandyTest.java
+++ src/test/java/com/aprilhoward/CandyTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 public class CandyTest {
     /**
-     * Methods under test:
+     * Methods tests
      *
      * <ul>
      *   <li>{@link Candy#Candy(String, String, BigDecimal)}
