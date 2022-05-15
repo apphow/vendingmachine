edf52c96ddd31b8563c2b9dd4dd6d3e08035817d
April Howard <apehow111@gmail.com>
2022-05-15 12:14:21 -0400

    CandyTest

---
+++ src/test/java/com/aprilhoward/CandyTest.java
@@ -0,0 +1,23 @@
+package com.aprilhoward;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+public class CandyTest {
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>{@link Candy#Candy(String, String, BigDecimal)}
+     *   <li>{@link Candy#getSound()}
+     * </ul>
+     */
+    @Test
+    public void testConstructor() {
+        assertEquals("Munch Munch, Yum!", (new Candy("Slot", "Name", BigDecimal.valueOf(42L))).getSound());
+    }
+}
+
